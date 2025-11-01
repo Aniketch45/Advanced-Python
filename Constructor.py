49017f0a4aed36bd12fcbd1fb492e2347debd8c9
Aniketch45 <aniketchafekar2004@gmail.com>
2025-11-01 19:18:52 +0530

    Learned Instance variable accessing Deleting and constructor Types

--- Constructor.py
+++ Constructor.py
@@ -1,30 +1,31 @@
-class Test:
-    def __init__(self):
-        print("this is constructor")
+# class Test:
+#     def __init__(self):
+#         print("this is constructor")
 
-t1=Test()
-t1.__init__()
+# t1=Test()
+# t1.__init__()   #we can call constructor explicitly
 
 
 
 # class Test:
-#     def __init__(self,x,y):
-#         self.a=x
-#         self.b=y
-#
 #     def __init__(self):
 #         print("this is constructor")
 #         self.a=20
 #         self.b=30
-#
+
+#     def __init__(self,x,y):   # Constructor Overloading and Overiding is not allowed in  python
+#         self.a=x
+#         self.b=y
+
+   
 #     def add(self):
 #         print("sum=",self.a+self.b)
-#
+
 #     def sub(self):
 #         print("sub=",self.a-self.b)
-#
+
 # #t1=Test()
-# t1=Test()
+# t1=Test(10,20)
 # t1.sub()
 # t1.add()
 
@@ -32,3 +33,19 @@ t1.__init__()
 
 # t2=Test()
 # t3=Test()
+
+class Test:
+    def __init__(self,name,age,address):
+        self.name=name
+        self.age=age
+        self.address=address
+    def __init__(self,name,age,address,status):
+        self.name=name
+        self.age=age
+        self.address=address
+        self.status=status
+    def show(self):
+        print(f"my name is :{self.name}, im {self.age} Years of age and im {self.address} Lives")
+
+t1=Test("Aniket",21,"Pune")
+t1.show()
