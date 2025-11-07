1b3d21457fa621229767ad883862390ac9f894ef
Aniketch45 <aniketchafekar2004@gmail.com>
2025-11-07 18:15:09 +0530

    Learned Inheritace types and solved examples

---
+++ inheritance_ex.py
@@ -0,0 +1,75 @@
+class Employee:
+    def __init__(self,id,name,address,sal=0):
+        self.id=id
+        self.name=name
+        self.address=address
+        self.sal=sal
+    
+    def show(self):
+        print("Employee Name is : ",self.name)
+        print("Employee Id is : ",self.id)
+        print("Address : ",self.address)
+        print("salary is : ",self.sal)
+
+class Fulltime(Employee):
+    def fmethod(self):
+        print("Employee salary is : ",self.sal)
+
+class Parttime(Employee):
+    hrate=500
+    def __init__(self, id, name, address,hours,sal=0):
+        super().__init__(id, name, address, sal)
+        self.hours=hours
+
+    def pmethod(self,):
+        self.sal+=self.hours*Parttime.hrate
+
+class Contract(Employee):
+    def __init__(self,id,name,address,month,sal):
+        super().__init__(id,name,address,sal)
+        self.month=month
+
+    def cmethod(self):
+        self.sal*=self.month
+
+ch=input("Enter your type of work : ")
+if ch=="fulltime":
+    f1=Fulltime(101,"Aniket","vaijapur",24000)
+    f1.fmethod()
+    f1.show()
+elif ch=="parttime":
+    p1=Parttime(101,"Aniket","vaijapur",34)
+    p1.pmethod()
+    p1.show()
+elif ch=="contract":
+    c1=Contract(101,"Aniket","vaijapur",12,6000)
+    c1.cmethod()
+    c1.show()
+else:
+    print("Invalid Choice")
+
+'''     
+class RBI:
+    def __init__(self,name,accountnum):
+        self.name=name
+        self.accountnum=accountnum
+    
+    def show(self):
+        print("Account Holder Name is :",self.name)
+
+class SBI(RBI):
+    def __init__(self, name, accountnum,branch):
+        super().__init__(name, accountnum)
+        self.branch=branch
+
+    def sbimethod(self):
+        pass
+
+class MGB(RBI):
+    def __init__(self, name, accountnum,branch):
+        super().__init__(name, accountnum)
+        self.branch=branch
+
+    def mgbmethod(self):
+        pass
+'''  
--- inheritance_types.py
+++ inheritance_types.py
@@ -1,1 +1,79 @@
-class
+#hierarchical inheritance
+
+# class Root:
+#     def __init__(self,x,y):
+#         self.x=x
+#         self.y=y
+
+#     def rmethod(self):
+#         print(f"x : {self.x}, y : {self.y}")
+
+# class Branch1(Root):
+#     def branch1m(self):
+#         print("Add :",self.x+self.y)
+
+# class Branch2(Root):
+#     def branch2m(self):
+#         print("mul : ",self.x*self.y)
+
+# r=Branch1(30,40)
+# r.rmethod()
+# r.branch1m()
+
+# r2=Branch2(30,40)
+# r2.rmethod()
+# r2.branch2m()
+
+
+
+#multi level inheritance
+# class grandparent():
+#     def __init__(self):
+#         print("grand parent Const")
+    
+#     def gshow(self):
+#         print("Grand parent method")
+    
+# class parent(grandparent):
+#     def __init__(self):
+#         super().__init__()
+#         print("parent Const")
+    
+#     def pshow(self):
+#         print("parent method")
+
+# class child(parent):
+#     def __init__(self):
+#         super().__init__()
+#         print("child Const")
+    
+#     def cshow(self):
+#         print("child method")
+
+# c=child()
+# c.gshow()
+# c.pshow()
+# c.cshow()
+
+
+
+#single level inheritance
+# class parent():
+#     def __init__(self):
+#         print("parent Const")
+    
+#     def pshow(self):
+#         print("parent method")
+
+# class child(parent):
+#     def __init__(self):
+#         super().__init__()
+#         print("child Const")
+    
+#     def cshow(self):
+#         print("child method")
+
+# c=child()
+# c.pshow()
+# c.cshow()
+
