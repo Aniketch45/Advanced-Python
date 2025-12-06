8faa93ebceab47a53250e4b38bb7fae2a413f744
Aniketch45 <aniketchafekar2004@gmail.com>
2025-12-06 23:59:20 +0530

    ex of multithreading

--- multithreading2.py
+++ multithreading2.py
@@ -1,21 +1,38 @@
+# from threading import *
+# import time
+# class Mythread(Thread):
+#     def run(self):
+#         print(current_thread())
+
+#     def display(self):
+#         for i in range(3):
+#             print(current_thread())
+#             print("hello Develo")
+#             time.sleep(2)
+
+# m2=Mythread()
+# th=Thread(target=m2.display)
+# th.start()
+# m2.start()
+
+# for i in range(4):
+#     print("main Thread")
+#     time.sleep(2)
+
 from threading import *
 import time
-class Mythread(Thread):
-    def run(self):
-        print(current_thread())
+class Demo():
+    def func(self):
+        print("Hello Welcome")
+        time.sleep(2)
 
-    def display(self):
-        for i in range(3):
-            print(current_thread())
-            print("hello Develo")
-            time.sleep(2)
-
+    def func2(self):
+        print("Exiting")
+        time.sleep(2)
+    
 m2=Mythread()
-th=Thread(target=m2.display)
-th.start()
-m2.start()
+# th=Thread(target=m2.display)
+# th.start()
+# m2.start()
 
-for i in range(4):
-    print("main Thread")
-    time.sleep(2)
 
