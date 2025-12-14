1065b6d40f5a59bf9ce037953343529e6838bfe1
Aniketch45 <aniketchafekar2004@gmail.com>
2025-12-14 19:05:31 +0530

    reversed words

---
+++ reverse_words.py
@@ -0,0 +1,8 @@
+def reverse_words(text):
+    words = text.split()
+    reversed_words = words[::-1]
+    return " ".join(reversed_words)
+
+
+data="Hello i am Learning Python"
+print(reverse_words(data))
