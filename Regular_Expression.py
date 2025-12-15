dd6ce3a70def1b919e8b5405cb407e78798cda2b
Aniketch45 <aniketchafekar2004@gmail.com>
2025-12-15 16:13:01 +0530

    hu

--- Regular_Expression.py
+++ Regular_Expression.py
@@ -1,9 +1,13 @@
 import re
-str="Python is object orientd language, easy to use"
-pattern=re.compile("Python")
-matcher=pattern.finditer(str)
+str = "python is object orientd python language, pythoneasy to use"
+pattern = re.compile("python")
+matcher = re.finditer(pattern,str)
+count = 0                                 
 for m in matcher:
-    print("Start index:",m.start())
-    print("matched pattern",m.group())
-    print("end index:",m.end())
+    count += 1
+    print("Start index:", m.start())
+    print("matched pattern", m.group())
+    print("end index:", m.end())
 
+print("python is occur", count,"times")
+                                 
